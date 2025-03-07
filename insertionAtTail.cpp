a0bdad10334464b6a0d1e7e4b18413dd688a23d8
Prikshit Sharma <prikshitcodes@gmail.com>
2025-03-07 10:42:03 +0530

    Create insertionAtTail.cpp

---
+++ insertionAtTail.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        // Code here
+        
+        if(head ==NULL){
+            
+        return  new  Node(x);
+        }
+        Node* temp = head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        Node* newNode = new Node(x);
+        temp->next=newNode;
+        return head;
+    }
+};
